82008e5402123ed1bcb80c0c42d3071733e705dd
mattgilgo <70973398+mattgilgo@users.noreply.github.com>
2022-08-04 21:56:46 -0400

    added multiple plots into json format. need to finish stacked bar plot

--- test.py
+++ test.py
@@ -7,44 +7,98 @@ datasets = \
 {
     "datasets": [
         {
+            "paths": "price_by_zipcode_weekoverweek_deltas.parquet",
+            "plots": 
+                {
+                    "plot_type": "choropleth",
+                    "args": {
+                        "title": "Total Price Changes W/W",
+                        "locations":'zipcode'
+                        "color": "total_price_delta_pct",
+                        "color_continuous_scale": "RdYlGn",
+                        "featureidkey": "properties.ZCTA5CE10",
+                        "range_color": [-100,100],
+                        "scope": "usa"
+                    }
+                },
+            "paths": "occupancy_by_zipcode_weekoverweek_deltas.parquet",
+            "plots":
+                {
+                    "plot_type": "choropleth",
+                    "args": {
+                        "title": "Occupancy Rate Changes W/W",
+                        "locations":'zipcode'
+                        "color": "available_delta_pct",
+                        "color_continuous_scale": "RdYlGn",
+                        "featureidkey": "properties.ZCTA5CE10",
+                        "range_color": [-100,100],
+                        "scope": "usa"
+                    }
+                }
+            
+        },      {
+            "paths": "price_by_zipcode_and_dayofweek.parquet",
+            "plots": 
+                {
+                    "plot_type": "line",
+                    "args": {
+                        "title": "Price Rate Changes W/W Monday-Sunday",
+                        "x": "day_of_week",
+                        "y": "total_price_delta_pct",
+                        "color": "zipcode"
+                    }
+                },
+        }       {
+            "paths": "occupancy_by_zipcode_and_dayofweek.parquet",
+            "plots":
+                {
+                    "plot_type": "line",
+                    "args": {
+                        "title": "Occupancy Rate Changes W/W Monday-Sunday",
+                        "x": "day_of_week",
+                        "y": "available_delta_pct",
+                        "color": "zipcode"
+                    }
+                }
+                }
+            
+        },         {
             "paths": "price_by_zipcode.parquet",
             "plots": [
                 {
-                    "plot_type": "bar",
+                    "plot_type": "Table",
                     "args": {
-                        "title": "hello",
-                        "xlable": "some units"
-                    }
-                }
-            ]
-        },      {
-            "paths": "price_by_zipcode_and_dayofweek.parquet",
-            "plots": [
-                {
-                    "plot_type": "choroplath",
-                    "args": {
-                        "title": "hello",
-                        "xlable": "some units",
-                        "zips": "33326",
-                        "figsize": 55
-                    }
-                },
-                {
-                    "plot_type": "bar",
-                    "args": {
-                        "title": "hello",
-                        "xlable": "some units"
-                    }
-                }
-            ]
-        },         {
-            "paths": "price_by_guest_no.parquet",
-            "plots": [
-                {
-                    "plot_type": "bar",
-                    "args": {
-                        "title": "hello",
-                        "xlable": "some units"
+                        "columnwidth": 50,
+                        "columnorder": [0,1,2,3,4],
+                        "header": {
+                            "height": 40,
+                            "values": [['<b>City</b>'], ['<b>Availability</b>'], ['<b>Check-in Availability</b>'], ['<b>Check-out Availability</b>'],['<b>Bookable</b>']]
+                            "line": {
+                                "color": "rgb(50,50,50)"
+                            }
+                            "align": 'left'
+                            "font": {
+                                "color": "rgb(45,45,45)",
+                                "size": 14
+                            }
+                        },
+                        "cells" {
+                            "values": ["cities", "vals[0]", "vals[1]", "vals[2]", "vals[3]"],
+                            "line" : {
+                                "color": "#506784"
+                            },
+                            "align":"left",  
+                            "font": {
+                                "family": "Arial", 
+                                "size": 14, 
+                                "color":"conditional_red_or_green",
+                            },
+                            "format": ["None", ",.2f"],
+                            "height": 30,
+                            "fill": {
+                                "color":"rgb(245,245,245)"
+                            }
+                        }
                     }
                 }
             ]
@@ -94,15 +148,40 @@ datasets = \
             "paths": ["price_by_guest_no.parquet", "occupancy_by_guest_no.parquet"],
             "plots": [
                 {
-                    "plot_type": "bar",
+                    "plot_type": "Figure",
                     "args": {
-                        "title": "hello",
-                        "xlable": "some units"
+                        "data": {
+                            "traces": [
+                                "plot1": {
+                                    "plot_type": "Bar",
+                                    "title": "Total Price",
+                                    "x": "guest_no",
+                                    "y": "median_total_price",
+                                    "yaxis": "y",
+                                    "offsetgroup": 1
+                                },                                 
+                                
+                                "plot2": {
+                                    "plot_type": "Bar",
+                                    "title": "Occupancy Rate",
+                                    "x": "guest_no",
+                                    "y": "occupancy_rate",
+                                    "yaxis": "y2",
+                                    "offsetgroup": 2
+                                }
+                            ],
+                        "layout": {
+                                    'xaxis': {'title': '# of Guests'},
+                                    'yaxis': {'title': 'Total Price'},
+                                    'yaxis2': {'title': 'Occupancy Rate', 'overlaying': 'y', 'side': 'right'}
+                            }
+                        },
+                        "title": "Median Price and Occupancy by # of Guests",
+                        "location": ""
                     }
                 }
             ]
         }
-
     ]
 }
 
